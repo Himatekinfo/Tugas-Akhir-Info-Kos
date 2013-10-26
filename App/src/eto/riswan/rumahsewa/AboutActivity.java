f37953bb74c54f5299a1113cb6ca28f88c6a8f9b
Syakur Rahman <bS.xx7_37@yahoo.com>
2013-10-26 09:52:27 +0700

    Security by obfuscation for login screen.

--- App/src/eto/riswan/rumahsewa/AboutActivity.java
+++ App/src/eto/riswan/rumahsewa/AboutActivity.java
@@ -1,8 +1,15 @@
 package eto.riswan.rumahsewa;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutActivity extends MainActivity {
+	public void copyright_onClick(View v) {
+		Intent x = new Intent(AboutActivity.this, LoginActivity.class);
+		this.startActivity(x);
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
